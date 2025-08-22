c614885ca14528bc6b3c9998d92e1f5f36e6bd04
Shalindra-coder <shalindraaporiya23@navgurukul.org>
2025-08-21 22:47:16 -0800

    fixed schdular issue overide core method

--- v3bid/__init__.py
+++ v3bid/__init__.py
@@ -1,1 +1,22 @@
 __version__ = "0.0.1"
+
+
+
+
+import erpnext.crm.doctype.email_campaign.email_campaign as email_campaign_module
+from v3bid.v3bid.custom_email_send import (
+    custom_send_email_to_leads_or_contacts,
+    custom_send_mail,
+)
+
+
+def patch_email_campaign():
+    print(" Patching Email Campaign Module with Custom Methods...----------------------------------------------------------")
+    email_campaign_module.send_email_to_leads_or_contacts = custom_send_email_to_leads_or_contacts
+    email_campaign_module.send_mail = custom_send_mail
+
+
+# Call patch function when app loads
+patch_email_campaign()
+
+
